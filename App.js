cdc31f386c54b8ab09e2259da232eb73f6413f6a
Brandon Michaels <brandonmichaels@Cliffords-MacBook-Pro.local>
2021-01-27 20:24:40 -0500

    Add starter screens

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.paragraph}>Test Screen</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +18,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+ 
+  paragraph: {
+    fontSize: 30,
+  }
 });
+
+            
