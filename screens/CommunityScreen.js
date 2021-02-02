305f4a6cc85c3f1abcb09395202351ce2b3af1b7
Brandon Michaels <brandonmichaels@Cliffords-MacBook-Pro.local>
2021-02-01 20:00:12 -0500

    Update community styles

--- screens/CommunityScreen.js
+++ screens/CommunityScreen.js
@@ -10,17 +10,29 @@ const windowHeight = Dimensions.get("window").height;
 
 export default class Community extends React.Component {
     state = {
-        searchText: ''
+        searchText: '', 
+        count: '',
     };
 
     _handleTextChange = searchText => {
         this.setState({ searchText });
     };
+
+    onPress = () => {
+      this.setState({
+        count: this.state.count + 1
+      });
+    };
   
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.header}>Time</Text>
+                <View style={styles.textContainer}>
+                  <Text style={styles.header}>Time</Text>
+                </View>
+
+                <View style={styles.circle}><Text style={styles.iconText}>$</Text></View>
+                <View style={styles.circle}><Text style={styles.iconText}>P</Text></View>
                 <StatusBar style="auto" />
 
                 <View style={styles.searchBar}>
@@ -41,18 +53,26 @@ export default class Community extends React.Component {
                         />
                     </TouchableHighlight>
                 </View>
+                    
+                      <View style={styles.buttonRecommendContainer}>
+                          <Text style={styles.buttonRecommend}>Recommended</Text>
+                      </View>
+                      
+                      <View style={styles.circle}><Text style={styles.iconText}>+</Text></View>
 
-                <TouchableOpacity onPress={() => this.props.navigation.navigate('aboutScreen')}>
-                    <View style={styles.bannerContainer}>
-                        <Text>Recommended</Text>
-                        <Image
-                            source={ require('../assets/images/banner.png') }
-                            style={styles.bannerImage}
-                        />
-                    </View>
-                </TouchableOpacity>
-            </View>
-        
+                      <View style={styles.buttonJoinContainer}>
+                          <Text style={styles.buttonJoin}>Join</Text>
+                      </View>
+
+                      <Text style={styles.header}>My Communities</Text>
+
+                      <View style={styles.communityContainer}>
+                        <Text style={styles.communityHeader}>Community 1 Name <View style={styles.circle}></View></Text>
+                        <Text style={styles.communityHeader}>Community 2 Name <View style={styles.circle}></View></Text>
+                        <Text style={styles.communityHeader}>Community 3 Name <View style={styles.circle}></View></Text>
+                        <Text style={styles.communityHeader}>Community 4 Name <View style={styles.circle}></View></Text>
+                      </View>
+            </View>        
         
         );
     }  
@@ -61,25 +81,24 @@ export default class Community extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50, 
+    marginLeft: 20
   },
-  paragraph: {
-    fontSize: 20,
-    alignItems: 'start'
+  header: {
+    fontSize: 30,
+    color: 'black',
+    fontFamily: 'Times New Roman'
   }, 
   searchBar: {
     width: windowHeight / 41,
     backgroundColor: 'white'
   }, 
   magnifyGlass: {
-    backgroundColor: 'blue',
+    backgroundColor: 'black',
     height: 35, 
     width: 35, 
     borderTopRightRadius: 10, 
     borderBottomRightRadius: 10, 
-    marginLeft: 200, 
   }, 
 searchBox: {
     width: 200, 
@@ -89,8 +108,58 @@ searchBox: {
     borderWidth: 1, 
     borderTopLeftRadius: 10, 
     borderBottomLeftRadius: 10,
-    marginTop: 50
   }, 
+buttonRecommendContainer: {
+  padding: 10,
+  width: 155,
+  backgroundColor: 'black', 
+},
+buttonJoinContainer: {
+  padding: 10,
+  width: 155,
+  backgroundColor: 'black',
+  marginBottom: 5,
+},
+buttonRecommend: {
+  fontSize: 20,
+  alignItems: 'center', 
+  color: 'white',
+  textAlign: 'center', 
+}, 
+buttonJoin: {
+  fontSize: 20,
+  alignItems: 'center', 
+  color: 'white',
+  textAlign: 'center', 
+},
+communityContainer: {
+  backgroundColor: 'lightgreen',
+  padding: 5, 
+  borderWidth: 1, 
+  borderColor: 'black',
+  width: windowWidth - 50,
+  marginTop: 5
+},
+communityHeader: {
+  fontSize: 20,
+  color: 'black',
+  borderColor: 'black',
+  borderWidth: 1,
+  padding: 15,
+}, 
+circle: {
+  width: 30,
+  height: 30,
+  borderRadius: 30 / 2,
+  backgroundColor: 'lightblue',
+  margin: 'auto',
+},
+iconText: {
+  textAlign: 'center', 
+  fontSize: 20,
+}, 
+textContainer: {
+  marginRight: 10,
+}
 });
 
-            
