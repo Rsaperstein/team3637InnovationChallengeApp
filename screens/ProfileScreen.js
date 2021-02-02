11233bc83d0b5a1d47809a9fd629ab0c94dc53e3
Rsaperstein <rsaperstein@hcrhs.org>
2021-02-01 21:14:07 -0500

    Fixed Krish's code

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Proflie</Text>
-      <View style={styles.circle}><Text style={styles.iconText}>'import portfolio pic'</Text></View>
-            </View>        
-  );
+export default class Profile extends React.Component{
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Proflie</Text>
+        <View style={styles.circle}><Text style={styles.iconText}>'import portfolio pic'</Text></View>
+      </View>        
+    );
+  }
 }
 
 const styles = StyleSheet.create({
