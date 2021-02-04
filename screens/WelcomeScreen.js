7c6d7f686d4a117c2ea403e0447cc59064978e03
Brandon-Michaels <59707657+Brandon-Michaels@users.noreply.github.com>
2021-02-03 20:49:36 -0500

    Styles to WelcomeScreen.js

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,12 +1,19 @@
 import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 import Constants from 'expo-constants';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.paragraph}>
-        Welcome Screen
+      
+      <Text style={styles.heading}>
+        (App Name)
+      </Text>
+      <Text style={styles.subheading}>
+        Welcome to the fitness app! Here is the welcome screen with filler text. 
+      </Text>
+      <Text style={styles.welcomeButton}>
+        Continue
       </Text>
     </View>
   );
@@ -19,11 +26,37 @@ const styles = StyleSheet.create({
     paddingTop: Constants.statusBarHeight,
     backgroundColor: '#ecf0f1',
     padding: 8,
+    alignItems: 'center'
   },
-  paragraph: {
+  heading: {
     margin: 24,
-    fontSize: 18,
+    fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'center',
+    position: 'relative',
+    top: 100
+  },
+  image: {
+    width: 100,
+    height: 100,
+    zIndex: 0
+  },
+  welcomeButton: {
+    padding: 10,
+    width: 150,
+    textAlign: 'center',
+    borderRadius: 20,
+    backgroundColor: 'rgb(124, 166, 229)',
+    color: 'white',
+    position: 'relative',
+    top: 100,
+  },
+  subheading: {
+    margin: 24,
+    fontSize: 16,
+    textAlign: 'center',
+    position: 'relative',
+    top: 80
   },
 });
+
