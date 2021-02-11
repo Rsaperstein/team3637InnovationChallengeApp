2e66925607cf78764adeb721f92c3c883431bb21
andlomb <59770182+andlomb@users.noreply.github.com>
2021-02-10 19:50:53 -0500

    Update ProgressBar.js

--- screens/ProgressBar.js
+++ screens/ProgressBar.js
@@ -60,7 +60,205 @@ const styles = StyleSheet.create({
  },
  ProgressBar: {
   height: 20,
-  width: 30,
+  width: 500,
+  backgroundColor: 'white',
+  borderColor: '#000',
+  borderWidth: 2,
+  borderRadius: 5,
+}
+});
+
+//Progress Bar 2 (To get more, Copy and Paste from here)
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+  //Using the Running Count to move the Progress Bar
+
+  var CountRun = 27;
+  
+  const [progress, setProgress] = useState(0);
+     setProgress(progress = CountRun);
+//I changed this to useState. Hoepfully it does something
+ const App = () => {
+  let animation = useRef(new Animated.Value(0));
+}
+useEffect(() => {
+  Animated.timing(animation.current, {
+    toValue: progress,
+    duration: 100
+  }).start();
+},[progress])
+const width = animation.current.interpolate({
+  inputRange: [0, 100],
+  outputRange: ["0%", "100%"],
+  extrapolate: "clamp"
+})
+}
+
+export default class ProgressBar extends React.Component {
+
+ render() { 
+   return (
+  <View style={styles.container}>
+  <Text>
+    Loading.....
+  </Text>
+  <View style={styles.ProgressBar}>
+  <Animated.View style={[StyleSheet.absoluteFill], {backgroundColor: "#8BED4F"}}/>
+</View>
+<Text>27</Text>
+</View>
+ );
+}
+}
+
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
+   paddingTop: Constants.statusBarHeight,
+   backgroundColor: '#ecf0f1',
+   padding: 8,
+ },
+ ProgressBar: {
+  height: 20,
+  width: 500,
+  backgroundColor: 'white',
+  borderColor: '#000',
+  borderWidth: 2,
+  borderRadius: 5,
+}
+});
+
+//Progress Bar 3 (To get more, Copy and Paste from here)
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+  //Using the Running Count to move the Progress Bar
+
+  var CountRun = 27;
+  
+  const [progress, setProgress] = useState(0);
+     setProgress(progress = CountRun);
+//I changed this to useState. Hoepfully it does something
+ const App = () => {
+  let animation = useRef(new Animated.Value(0));
+}
+useEffect(() => {
+  Animated.timing(animation.current, {
+    toValue: progress,
+    duration: 100
+  }).start();
+},[progress])
+const width = animation.current.interpolate({
+  inputRange: [0, 100],
+  outputRange: ["0%", "100%"],
+  extrapolate: "clamp"
+})
+}
+
+export default class ProgressBar extends React.Component {
+
+ render() { 
+   return (
+  <View style={styles.container}>
+  <Text>
+    Loading.....
+  </Text>
+  <View style={styles.ProgressBar}>
+  <Animated.View style={[StyleSheet.absoluteFill], {backgroundColor: "#8BED4F"}}/>
+</View>
+<Text>27</Text>
+</View>
+ );
+}
+}
+
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
+   paddingTop: Constants.statusBarHeight,
+   backgroundColor: '#ecf0f1',
+   padding: 8,
+ },
+ ProgressBar: {
+  height: 20,
+  width: 500,
+  backgroundColor: 'white',
+  borderColor: '#000',
+  borderWidth: 2,
+  borderRadius: 5,
+}
+});
+
+//Progress Bar 4 (To get more, Copy and Paste from here)
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+  //Using the Running Count to move the Progress Bar
+
+  var CountRun = 27;
+  
+  const [progress, setProgress] = useState(0);
+     setProgress(progress = CountRun);
+//I changed this to useState. Hoepfully it does something
+ const App = () => {
+  let animation = useRef(new Animated.Value(0));
+}
+useEffect(() => {
+  Animated.timing(animation.current, {
+    toValue: progress,
+    duration: 100
+  }).start();
+},[progress])
+const width = animation.current.interpolate({
+  inputRange: [0, 100],
+  outputRange: ["0%", "100%"],
+  extrapolate: "clamp"
+})
+}
+
+export default class ProgressBar extends React.Component {
+
+ render() { 
+   return (
+  <View style={styles.container}>
+  <Text>
+    Loading.....
+  </Text>
+  <View style={styles.ProgressBar}>
+  <Animated.View style={[StyleSheet.absoluteFill], {backgroundColor: "#8BED4F"}}/>
+</View>
+<Text>27</Text>
+</View>
+ );
+}
+}
+
+const styles = StyleSheet.create({
+ container: {
+   flex: 1,
+   justifyContent: 'center',
+   alignItems: 'center',
+   paddingTop: Constants.statusBarHeight,
+   backgroundColor: '#ecf0f1',
+   padding: 8,
+ },
+ ProgressBar: {
+  height: 20,
+  width: 500,
   backgroundColor: 'white',
   borderColor: '#000',
   borderWidth: 2,
