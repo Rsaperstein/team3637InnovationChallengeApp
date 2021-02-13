38ece74846da6a8e322733d4466f59c0c7957470
andlomb <59770182+andlomb@users.noreply.github.com>
2021-02-13 09:37:15 -0500

    Update ProgressBar.js

--- screens/ProgressBar.js
+++ screens/ProgressBar.js
@@ -43,19 +43,19 @@ export default class ProgressBar extends React.Component {
   <View style={styles.ProgressBar}>
   <Animated.View style={[StyleSheet.absoluteFill], {backgroundColor: "#8BED4F"}}/>
 </View>
-<Text>27</Text>
+<Text>27%</Text>
 <View style={styles.ProgressBar}>
   <Animated.View style={[StyleSheet.absoluteFill], {backgroundColor: "#8BED4F"}}/>
 </View>
-<Text>27</Text>
+<Text>27%</Text>
 <View style={styles.ProgressBar}>
   <Animated.View style={[StyleSheet.absoluteFill], {backgroundColor: "#8BED4F"}}/>
 </View>
-<Text>27</Text>
+<Text>27%</Text>
 <View style={styles.ProgressBar}>
   <Animated.View style={[StyleSheet.absoluteFill], {backgroundColor: "#8BED4F"}}/>
 </View>
-<Text>27</Text>
+<Text>27%</Text>
 </View>
  );
 }
