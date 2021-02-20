1ccc296891ab1aef9fb5f48071f7e5bd8ad24b47
andlomb <59770182+andlomb@users.noreply.github.com>
2021-02-20 09:22:09 -0500

    Update ProgressBar.js

--- screens/ProgressBar.js
+++ screens/ProgressBar.js
@@ -88,7 +88,7 @@ const styles = StyleSheet.create({
 PercentU: {
   height: 20,
   width: 248,
-  backgroundColor: 'cyan',
+  backgroundColor: '#172A3A',
   borderColor: '#000',
   borderWidth: 2,
   borderRadius: 5,
@@ -96,7 +96,7 @@ PercentU: {
 PercentD: {
   height: 20,
   width: 197,
-  backgroundColor: 'cyan',
+  backgroundColor: '#172A3A',
   borderColor: '#000',
   borderWidth: 2,
   borderRadius: 5,
@@ -104,7 +104,7 @@ PercentD: {
 PercentT: {
   height: 20,
   width: 97,
-  backgroundColor: 'cyan',
+  backgroundColor: '#172A3A',
   borderColor: '#000',
   borderWidth: 2,
   borderRadius: 5,
@@ -112,7 +112,7 @@ PercentT: {
 PercentC: {
   height: 20,
   width: 314,
-  backgroundColor: 'cyan',
+  backgroundColor: '#172A3A',
   borderColor: '#000',
   borderWidth: 2,
   borderRadius: 5,
