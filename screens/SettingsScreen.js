95c893403b9bec71246e77c1897ea13fce733894
andlomb <59770182+andlomb@users.noreply.github.com>
2021-02-20 10:06:53 -0500

    Update SettingsScreen.js

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -104,7 +104,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     padding: 10,
     borderRadius: 15,
-    width: (windowWidth/1.5),
+    width: (windowWidth/1.2),
     fontSize: 32,
     marginBottom: 20,
   },
